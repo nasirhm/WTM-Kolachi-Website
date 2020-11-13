8dd9ef41726a67f0fe5b3812d9a0ca59a12a5b6d
HiraTariq-01 <56246308+HiraTariq-01@users.noreply.github.com>
2020-11-14 03:50:41 +0500

    :wrench: Modified Social Media Links to WTM Kolachi links (#9)
    Modified Social Media links from WTM Algiers to WTM Kolachi's

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -10,19 +10,19 @@ const SocialMedia = ({ direction, twitter_link, facebook_link, linkedin_link, gi
 
     <div>
 
-        <a target="_blank" rel="noopener noreferrer" href={twitter_link !== "" && twitter_link !== undefined ? twitter_link : "https://twitter.com/AlgiersWTM"}>
+        <a target="_blank" rel="noopener noreferrer" href={twitter_link !== "" && twitter_link !== undefined ? twitter_link : "https://twitter.com/WTMKolachi"}>
             <img src={twitter} className={"Social-Media-Button " + direction} alt={"Visit us on Twitter"} />
         </a>
 
-        <a target="_blank" rel="noopener noreferrer" href={facebook_link !== "" && facebook_link !== undefined ? facebook_link : "https://fr-fr.facebook.com/WomenTechmakersAlgiers/"}>
+        <a target="_blank" rel="noopener noreferrer" href={facebook_link !== "" && facebook_link !== undefined ? facebook_link : "https://www.facebook.com/WTMKolachi"}>
             <img src={facebook} className={"Social-Media-Button " + direction} alt={"Visit us on facebook"} />
         </a>
 
-        <a target="_blank" rel="noopener noreferrer" href={github_link !== "" && github_link !== undefined ? github_link : "https://github.com/WTM-Algiers"}>
+        <a target="_blank" rel="noopener noreferrer" href={github_link !== "" && github_link !== undefined ? github_link : "https://github.com/WTM-Kolachi"}>
             <img src={github} className={"Social-Media-Button " + direction} alt={"Visit us on GitHub"} />
         </a>
 
-        <a target="_blank" rel="noopener noreferrer" href={linkedin_link !== "" && linkedin_link !== undefined ? linkedin_link : "https://www.linkedin.com/company/wtm-algiers/"}>
+        <a target="_blank" rel="noopener noreferrer" href={linkedin_link !== "" && linkedin_link !== undefined ? linkedin_link : "https://www.linkedin.com/company/wtmkolachi/"}>
             <img src={linkedin} className={"Social-Media-Button " + direction} alt={"Visit us on LinkedIn"} />
         </a>
 
